87d0ac7f91bc0fb0715711a7b151f8bfd2947921
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-19 14:21:10 -0400

    Update meeting.js

--- meeting.js
+++ meeting.js
@@ -1,7 +1,5 @@
 //solution 1 and clever
 
-
-
 const countDevelopers = list => list.filter((obj) => obj.continent === 'Europe'&& obj.language === 'JavaScript').length;
 
 // solution 2 Meeting # 2
@@ -20,3 +18,14 @@ function greetDevelopers(list) {
 // solution 2 Meeting #2 Clever
 
 const greetDevelopers = list => list.map(dev => ({...dev, greeting: `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`}));
+
+
+// solution 3 Meeting # 3 Clever
+
+var list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+
+const isRubyComing = list => list.some(obj=>obj.language==='Ruby'); 
